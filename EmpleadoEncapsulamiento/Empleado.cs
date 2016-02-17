6996181933d5d189c9eca0dd0c9c8545da258ffe
hsabillon7 <h.hsabillon@gmail.com>
2016-02-17 08:21:47 -0600

    Creación de propiedades a través de .NET

---
+++ EmpleadoEncapsulamiento/Empleado.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpleadoEncapsulamiento
+{
+    class Empleado
+    {
+        // Variables miembro
+        private string nombreEmpleado;
+        private int idEmpleado;
+        private double salarioActual;
+        private int edadEmpleado;
+
+        /*
+        // Accessor (método get)
+        public string GetNombre()
+        {
+            return nombreEmpleado;
+        }
+
+        // Mutator (método set)
+        public void SetNombre(string nombre)
+        {
+            // Revisar el valor del nombre antes de asignarlo
+            if (nombre.Length >= 10)
+                nombreEmpleado = nombre;
+            else
+                Console.WriteLine("¡Error! El nombre no puede ser menor de 10 caracteres...");
+        }
+        */
+
+        // Agregando métodos set/get de la manera .NET
+        // a través de PROPIEDADES
+
+        public string Nombre
+        {
+            get { return nombreEmpleado; }
+            set
+            {
+                if (value.Length >= 10)
+                    nombreEmpleado = value;
+                else
+                    Console.WriteLine("¡Error! El nombre no puede ser menor de 10 caracteres...");
+            }
+        }
+
+        // Constructores
+        public Empleado() { }
+
+        public Empleado(string nombre)
+        {
+            nombreEmpleado = nombre;
+        }
+    }
+}
---
+++ EmpleadoEncapsulamiento/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpleadoEncapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Demostración del encapsulamiento *****");
+
+            // Crear objetos de tipo Empleado
+            Empleado empleado = new Empleado("Marvin");
+
+            // Demostración con métodos Accessor/Mutator
+            // Console.WriteLine("El nombre del empleado es {0}", empleado.GetNombre());
+
+            // Propiedades .NET
+            Console.WriteLine("El nombre del empleado es {0}", empleado.Nombre);
+
+            Empleado otroEmpleado = new Empleado();
+            // otroEmpleado.SetNombre("María");
+            // Console.WriteLine("El nombre del empleado es {0}", otroEmpleado.GetNombre());
+            otroEmpleado.Nombre = "María";
+            Console.ReadLine();
+        }
+    }
+}
